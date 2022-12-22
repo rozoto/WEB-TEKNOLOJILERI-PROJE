2072905c501a8bafeec5ef8ba73a5cb6bc8eecb0
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2022-12-22 21:45:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ 
+echo "Ad:  "+$_POST['adi'];
+?>
+
+<?php
+ 
+echo "Soyad:  "+$_POST['soyadi'];
+?>
+
+<?php
+ 
+echo "Cinsiyet:  "+$_POST['cinsiyet'];
+?>
+
+<?php
+ 
+echo "E-mail adresiniz:  "+$_POST['email'];
+?>
+
+<?php
+ 
+echo "Yaşadığı şehir:  "+$_POST['sehir'];
+?>
